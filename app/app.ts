278b8798d694c116245e3d8089d6db87a2e7aa8f
Diego Ortiz <dxortiz2@gmail.com>
2021-11-18 09:52:00 -0600

    creating interfaces

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,7 @@ function startGame() {
     logPlayer(playerName);
 
     postScore(100, playerName);
+    postScore(-5, playerName);
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -23,8 +24,38 @@ function getInputValue(elementID: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+    let logger: (value: string) => void;
+
+    if (score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage;
+    }
+
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
     scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+    console.log(err);
+}
+
+let myResult: Result = {
+    playerName: 'Marie',
+    score: 5,
+    problemCount: 5,
+    factor: 7
+};
+
+let player: Person = {
+    name: 'Daniel',
+    formatName: () => 'Dan'
+};
